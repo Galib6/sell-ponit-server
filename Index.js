0d5eaea00697fd774836726a835f8f3ac48b3674
Galib6 <asadullahalgalib81@gmail.com>
2022-11-28 23:04:20 +0600

    JWT

--- Index.js
+++ Index.js
@@ -16,6 +16,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 // console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// varify JWT
 function verifyJWT(req, res, next) {
 
     const authHeader = req.headers.authorization;
